737dd81a654e804dec2dc38f3cb45e0309d9a947
Hope <serieuxhope@prontonmail.ch>
2020-11-25 17:26:38 +0530

    Updates

---
+++ precache-manifest.c4fdd97510ed2180bff1a705b8c41717.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55bbdf5b534faf42bd003dedbee29aee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0c2912dde3b5c368b88",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "39341e9da1036258adf3",
+    "url": "/static/js/2.35fde9e2.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.35fde9e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c2912dde3b5c368b88",
+    "url": "/static/js/main.fb9bfeec.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "18320f8a9a89ce3842ed5014856d2f30",
+    "url": "/static/media/hashirshoaeb.18320f8a.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
